7d21243406c6253d063f87e3456abdce030255cd
Celso Junior <celsogubeal@gmail.com>
2022-07-09 16:17:48 -0300

    Added Conhecendo o styled components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
-import { Welcome } from './src/Components/Welcome';
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Welcome title='React Native Bare Workflow com Typescript'/>
-    </View>
+    <Dashboard/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
