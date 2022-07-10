b6f460742a1568d3d42a97d3d20b4e0a8e01337d
Celso Junior <celsogubeal@gmail.com>
2022-07-10 11:31:19 -0300

    Added Criacao dos cartoes de destaque

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -11,7 +11,8 @@ import {
   UserName,
   Icon
 } from "./styles";
-import { RFValue } from "react-native-responsive-fontsize";
+
+import { HighlightCard } from "../../components/HighlightCard";
 
 export function Dashboard(){
   return (
@@ -30,6 +31,8 @@ export function Dashboard(){
         <Icon name='power'/>
         </UserWrapper>
       </Header>
+
+      <HighlightCard />
     </Container>
   );
 }
